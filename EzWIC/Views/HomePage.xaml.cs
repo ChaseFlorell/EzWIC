df3b7c3e762cabd74b1a53f6d8cf9aac5820f352
Mark J Radacz <radacz.mcp@gmail.com>
2017-09-26 19:24:36 +0200

    test proj
    create HomePage.xaml to look like home.jpg

---
+++ EzWIC/Views/HomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EzWIC.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        void FoodSearch_Clicked(object sender, System.EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+		void FoodList_Clicked(object sender, System.EventArgs e)
+		{
+			//throw new NotImplementedException();
+		}
+		void WICCard_Clicked(object sender, System.EventArgs e)
+		{
+			//throw new NotImplementedException();
+		}
+		
+    }
+}
